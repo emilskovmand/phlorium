176038169a30eab80dcac90cf3cb8f171b572e4c
Emil Skovmand <emilskovmand@gmail.com>
2023-12-03 16:24:46 +0100

    carousel

---
+++ components/Carousel.tsx
@@ -0,0 +1,114 @@
+'use client'
+import { A11y, Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Box, Button, ButtonProps, Text, VStack } from '@chakra-ui/react';
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/solid';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+export interface ICarouselItem {
+  id: any
+  title: string
+  undertitle: string
+  imageUrl: string
+}
+
+interface ICarousel {
+  items?: ICarouselItem[]
+}
+
+const CarouselItem = ({ imageUrl, id, title, undertitle }: ICarouselItem) => {
+
+  return <Box
+    bgImage={imageUrl}
+    backgroundSize={"cover"}
+    backgroundPosition={"center"} h="155"
+    borderRadius={"12px"}
+  >
+    <VStack gap="0px" justifyContent={"flex-end"} h="100%" alignItems={"flex-start"} p="4">
+      <Text noOfLines={1} fontWeight={"semibold"} fontSize={"16px"} color="white.900">{title}</Text>
+      <Text noOfLines={1} fontWeight={"semibold"} fontSize={"10px"} color="white.900">{undertitle}</Text>
+    </VStack>
+  </Box>
+}
+
+/**
+  position: absolute;
+  top: var(--swiper-navigation-top-offset, 50%);
+  width: calc(var(--swiper-navigation-size) / 44 * 27);
+  height: var(--swiper-navigation-size);
+  margin-top: calc(0px - (var(--swiper-navigation-size) / 2));
+  z-index: 10;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  right: var(--swiper-navigation-sides-offset, 10px);
+  left: auto;
+ */
+
+const buttonStyles: ButtonProps = {
+  position: "absolute",
+  top: "var(--swiper-navigation-top-offset, 50%)",
+  width: "calc(var(--swiper-navigation-size) / 44 * 27)",
+  height: "var(--swiper-navigation-size)",
+  marginTop: "calc(0px - (var(--swiper-navigation-size) / 2))",
+  zIndex: 10,
+  cursor: "pointer",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  bgColor: "black.900",
+  borderRadius: "40px",
+  _hover: {
+    bgColor: "black.900"
+  },
+  _disabled: { display: "none" }
+}
+
+const rightButtonStyle: ButtonProps = {
+  ...buttonStyles,
+  right: "var(--swiper-navigation-sides-offset, 10px)",
+  left: "auto",
+}
+
+const leftButtonStyle: ButtonProps = {
+  ...buttonStyles,
+  right: "auto",
+  left: "var(--swiper-navigation-sides-offset, 10px)",
+}
+
+export const Carousel = ({ items }: ICarousel) => {
+
+  return <Swiper
+    modules={[Navigation, A11y]}
+    spaceBetween={16}
+    slidesPerView={4.5}
+    navigation={true ? { prevEl: ".arrow-left", nextEl: ".arrow-right" } : true}
+    pagination={{ clickable: true }}
+    scrollbar={{ draggable: true }}
+    breakpoints={{
+      600: {
+        slidesPerView: 3
+      },
+      400: {
+        slidesPerView: 2
+      },
+      1000: {
+        slidesPerView: 4.5
+      }
+    }}
+    onSwiper={(swiper) => console.log(swiper)}
+    onSlideChange={() => console.log('slide change')}
+  >
+    {items?.map(v => <SwiperSlide key={v.id}><CarouselItem {...v} /></SwiperSlide>)}
+
+    <Button {...leftButtonStyle} className='arrow-left arrow'>
+      <ArrowLeftIcon width={"18px"} height={"18px"} />
+    </Button>
+    <Button {...rightButtonStyle} className='arrow-right arrow'>
+      <ArrowRightIcon width={"18px"} height={"18px"} />
+    </Button>
+  </Swiper>
+}
--- components/home/HomeWrapper.tsx
+++ components/home/HomeWrapper.tsx
@@ -1,10 +1,21 @@
 'use client'
-import { chakra } from "@chakra-ui/react"
+import { Box, chakra } from "@chakra-ui/react"
+import { Carousel, ICarouselItem } from "../Carousel"
 import { Welcome } from "./Welcome"
+
+const items: ICarouselItem[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((v) => ({
+  id: v,
+  imageUrl: "https://images.unsplash.com/photo-1682685797365-41f45b562c0a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  title: "En titel",
+  undertitle: "decal/coding"
+}))
 
 export const HomeWrapper = () => {
 
   return <chakra.main pl={"8"} pt="8" maxW="100%">
     <Welcome />
+    <Box mt="5" mb="6">
+      <Carousel items={items} />
+    </Box>
   </chakra.main>
 }
