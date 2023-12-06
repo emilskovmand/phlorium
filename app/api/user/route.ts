62259698da6618f1ab9fc7f212a47cd4321b374d
Christian Kirk Ludvigsen <christianludvigsendk@gmail.com>
2023-12-06 12:27:02 +0100

    maybe work? havent teseted

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,23 +1,36 @@
 import { connect } from "@/lib/db";
+import userModel from "@/models/user.model";
+import { Types } from "mongoose";
 
 interface IHandleSettings {
-    displayname?: string,
-    about?: string,
-    colormode?: string,
-    profilePicture?: Types.ObjectId | string,
+    id: string;
+    displayname?: string;
+    about?: string;
+    colormode?: string;
+    profilePicture?: Types.ObjectId | string;
 }
 
-async function HandleSettings({ displayname, about, colormode, profilePicture }: IHandleSettings) {
-    await connect()
+async function HandleSettings({ id, displayname, about, colormode, profilePicture }: IHandleSettings) {
+    const db = await connect();
 
-    const settings = {
-        displayname: displayname,
-        about: about,
-        colormode: colormode,
-        profilePicture: profilePicture
-    } as any
+    try {
+        const user = await userModel.findOneAndUpdate(
+            { _id: id },
+            {
+                $set: {
+                    displayname,
+                    about,
+                    colormode,
+                    profilePicture,
+                },
+            },
+            { new: true }
+        );
 
+    } catch (error) {
+        console.log('error');
+        return false;
+    }
 }
 
-export default HandleSettings
-
+export default HandleSettings;
