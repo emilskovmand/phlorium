6f6b51383918c988e14449a8c35b0b674ca1ee30
Emil <emilskovmand@gmail.com>
2023-12-06 11:43:54 +0100

    nice

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -1,13 +1,20 @@
 import { IPost } from "@/interfaces/post.interface";
 import { IUser } from "@/interfaces/user.interface";
-import { Box, Text, VStack } from "@chakra-ui/react";
+import { Box, HStack, Heading, Text, VStack } from "@chakra-ui/react";
 
+const PostDecal = ({ decal }: { decal?: string }) => {
 
-export const PostItem = ({ user }: IPost) => {
+  return <Box px="2" py="1" borderRadius={"12px"} bgColor="white.900" _dark={{ bgColor: "black.300" }}>
+    decal/{decal}
+  </Box>
+}
+
+export const PostItem = ({ title, text, user }: IPost) => {
 
   const postedBy = user as IUser
 
   return <VStack
+    gap="0px"
     alignItems={"flex-start"}
     borderRadius={"16px"}
     w="100%"
@@ -18,7 +25,15 @@ export const PostItem = ({ user }: IPost) => {
     justifyContent={"flex-start"}
   >
     <Box>
-      <Text>{postedBy.username}</Text>
+      <Text>{postedBy.username} &#x2022; 1 min ago</Text>
     </Box>
+    <Box py="4" maxH="100px">
+      <Heading fontSize="28px" fontWeight={"medium"} mb="2">{title}</Heading>
+      <Text fontSize="14px" noOfLines={2}>{text}</Text>
+    </Box>
+    <HStack>
+      <PostDecal decal="Coding" />
+      <PostDecal decal="Gaming" />
+    </HStack>
   </VStack>
 }
