7894fd0c913672baef0956c7a5e0c74773b3d61b
Emil Skovmand <emilskovmand@gmail.com>
2023-12-14 14:07:30 +0100

    easy

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,29 +2,35 @@ import { ISettingsForm } from "@/app/settings/page"
 import { connect } from "@/lib/db"
 import { getSession } from "@/lib/helpers/session"
 import userModel from "@/models/user.model"
+import _ from "lodash"
 import { NextRequest } from "next/server"
 
 export async function PATCH(req: NextRequest) {
-    await connect()
+  await connect()
 
-    const session = await getSession()
+  const session = await getSession()
 
-    if (!session) {
-        return Response.json({})
-    }
+  if (!session) {
+    return Response.json({})
+  }
 
-    const user = await userModel.findOne({ _id: session })
+  let user = await userModel.findOne({ _id: session })
 
-    const { settings } = (await req.json()) as ISettingsForm
+  const userUpdateForm = (await req.json()) as ISettingsForm
 
-    if (user) {
-        user.settings ||= {}
-        user.settings.displayname = settings?.displayname
-        user.settings.about = settings?.about
-        user.settings.colormode = settings?.colormode
-        await user.save()
-    }
+  if (user) {
+    user = _.merge(user || {}, _.omit(userUpdateForm, [
+      '_id',
+      'id',
+      'username',
+      'email',
+      'password',
+      'profileImages',
+      'googleCredentials',
+    ]))
+    await user.save()
+  }
 
-    return Response.json({ success: true })
+  return Response.json({ success: true })
 
 }
